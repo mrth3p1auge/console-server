1bb3ced1d8d292da9da1c00d27272fa8463626de
Chris Scutt <chris.scutt@gmail.com>
2019-05-09 00:18:18 -0400

    add the com-list.sh that will build the menu

---
+++ com-list.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#  This script is used to produce a list
+#   of avaiale minicom configs.
+#
+#         chris.scutt@gmail.com   
+#         02/17/2019
+
+
+# //// Define colors \\\\
+RED="\e[31m"
+BLUE="\e[34m"
+GREEN="\e[32m"
+GRAY="\e[37m"
+BOLD="\e[1m"
+DIM="\e[2m"
+RESET="\e[0m"
+
+OUTPUT="/tmp/com-menu.txt"
+
+exec &> ${OUTPUT}
+
+printf "\n"
+printf "\t $BOLD$BLUE Available \n"
+printf "\t------------$RESET\n"
+
+for o in  `ls /etc/minicom | awk -F "." '{print $2}'`; do
+        printf "\t$GREEN%s$RESET\n" "${o}"
+    done
+    
+    printf "\n"
+    
+    printf "$GRAY Example: minicom cisco $RESET\n"
+    printf "\n"
+    
+    
+    exec &> /dev/tty
